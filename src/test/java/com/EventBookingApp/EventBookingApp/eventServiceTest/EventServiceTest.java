fac81bfbcc977fb401ac1a0fa62e276b459b7922
Agboola Tobi Samuel <tobi4tee@gmail.com>
2024-04-13 17:13:31 +0100

    Add more eventService tests

--- src/test/java/com/EventBookingApp/EventBookingApp/eventServiceTest/EventServiceTest.java
+++ src/test/java/com/EventBookingApp/EventBookingApp/eventServiceTest/EventServiceTest.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class EventServiceTest {
@@ -35,8 +35,126 @@ public class EventServiceTest {
 
         EventCreationResponse response = eventService.createEvent(request);
 
-//        assertThat(response).isNotNull();
-        assertEquals(1, eventRepository.count());
+        assertThat(response).isNotNull();
+//        assertEquals(1, eventRepository.count());
 
     }
+
+
+    @Test
+    public void userCreatingMultipleEvents() throws EventAppException {
+        EventCreationRequest request = new EventCreationRequest();
+        request.setEmail("tobi099@gmail.com");
+        request.setName("Hackathon");
+        request.setDescription("A tech conference");
+        request.setCategory(EventCategory.CONCERT);
+        request.setNumberOfAttendees(700);
+        request.setEventDate("2025/12/16/12/00/00");
+
+        EventCreationResponse response = eventService.createEvent(request);
+
+        assertThat(response).isNotNull();
+//        assertEquals(1, eventRepository.count());
+
+    }
+
+    @Test
+    public void testThatIfANonUserAttemptsToCreateAnEventExceptionIsThrown(){
+        EventCreationRequest request = new EventCreationRequest();
+        request.setEmail("avy@gmail.com");
+        request.setName("SportBar");
+        request.setDescription("A game for kids...");
+        request.setCategory(EventCategory.GAME);
+        request.setNumberOfAttendees(500);
+        request.setEventDate("2024/08/20/12");
+        assertThrows(EventAppException.class,()->eventService.createEvent(request));
+
+    }
+
+
+    @Test
+    public void testThatIfEventNameIsMoreThan100CharactersExceptionIsThrown(){
+        EventCreationRequest request = new EventCreationRequest();
+        request.setEmail("avy@gmail.com");
+        request.setName("SportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBar");
+        request.setDescription("A game for kids...");
+        request.setCategory(EventCategory.GAME);
+        request.setNumberOfAttendees(500);
+        request.setEventDate("2024/08/20/12");
+        assertThrows(EventAppException.class,()->eventService.createEvent(request));
+
+    }
+
+
+    @Test
+    public void testThatIfEventDescriptionIsMoreThan1000CharactersExceptionIsThrown(){
+        EventCreationRequest request = new EventCreationRequest();
+        request.setEmail("avy@gmail.com");
+        request.setName("SportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBarSportBar");
+        request.setDescription("In today's fast-paced world, the convenience of booking systems has become an essential aspect of daily life. From booking tickets for a concert or reserving a spot at a conference, these systems are used widely by individuals and businesses alike.\n" +
+                "In today's fast-paced world, the convenience of booking systems has become an essential aspect of daily life. From booking tickets for a concert or reserving a spot at a conference, these systems are used widely by individuals and businesses alike.\n" +
+                "In today's fast-paced world, the convenience of booking systems has become an essential aspect of daily life. From booking tickets for a concert or reserving a spot at a conference, these systems are used widely by individuals and businesses alike.\n" +
+                "In today's fast-paced world, the convenience of booking systems has become an essential aspect of daily life. From booking tickets for a concert or reserving a spot at a conference, these systems are used widely by individuals and businesses alike.\n");
+        request.setCategory(EventCategory.GAME);
+        request.setNumberOfAttendees(500);
+        request.setEventDate("2024/08/20/12");
+        assertThrows(EventAppException.class,()->eventService.createEvent(request));
+
+    }
+
+    @Test
+    public void testThatIfEventDescriptionIsBlankExceptionIsThrown(){
+        EventCreationRequest request = new EventCreationRequest();
+        request.setEmail("avy@gmail.com");
+        request.setName("SportBar");
+        request.setDescription("");
+        request.setCategory(EventCategory.GAME);
+        request.setNumberOfAttendees(500);
+        request.setEventDate("2024/08/20/12");
+        assertThrows(EventAppException.class,()->eventService.createEvent(request));
+
+    }
+
+    @Test
+    public void testThatIfEventNameIsBlankExceptionIsThrown(){
+        EventCreationRequest request = new EventCreationRequest();
+        request.setEmail("avy@gmail.com");
+        request.setName("");
+        request.setDescription("A game for kids...");
+        request.setCategory(EventCategory.GAME);
+        request.setNumberOfAttendees(500);
+        request.setEventDate("2024/08/20/12");
+        assertThrows(EventAppException.class,()->eventService.createEvent(request));
+
+    }
+
+
+    @Test
+    public void testThatIfEventAttendeesIsMoreThan1000ExceptionIsThrown(){
+        EventCreationRequest request = new EventCreationRequest();
+        request.setEmail("avy@gmail.com");
+        request.setName("Bingo Game");
+        request.setDescription("A game for kids...");
+        request.setCategory(EventCategory.GAME);
+        request.setNumberOfAttendees(1500);
+        request.setEventDate("2024/08/20/12");
+        assertThrows(EventAppException.class,()->eventService.createEvent(request));
+
+    }
+
+
+    @Test
+    public void testThatIfNumberOfAvailableEventAttendeesIsLessThan1ExceptionIsThrown(){
+        EventCreationRequest request = new EventCreationRequest();
+        request.setEmail("avy@gmail.com");
+        request.setName("Bingo Game");
+        request.setDescription("A game for kids...");
+        request.setCategory(EventCategory.GAME);
+        request.setNumberOfAttendees(0);
+        request.setEventDate("2024/08/20/12");
+        assertThrows(EventAppException.class,()->eventService.createEvent(request));
+
+    }
+
+
 }
